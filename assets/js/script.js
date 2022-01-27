0844249de2c490223774bb006ce453decae4f347
Erwin <erwin.cca@gmail.com>
2022-01-27 17:08:26 -0400

    Avance N°2

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,25 +3,39 @@
 function cachipun(){
     //1.- Solicitar al usuario las veces que desea que se repita el juego.
     //Exigir que el valor ingresado sea un numero impar para poder obtener un vencedor.
+    var numJugadas;
     do {
-        var numJugadas=prompt("Ingrese el número de jugadas [debe ser un número impar]: ")
-        var numJugadas=parseInt(numJugadas)
-        if (numJugadas%2==0) {
-            alert("Ha ingresado un número par. Por favor, ingrese un número impar.")
+        numJugadas=prompt("Ingrese el número de jugadas [debe ser un número impar]: ")
+        numJugadas=parseInt(numJugadas)
+        if (numJugadas%2!=1) {
+            alert("Por favor, ingrese un número impar.")
         }
-    } while (numJugadas%2==0);
+    } while (numJugadas%2!=1);
     
     //2.- Ejecución del juego en sí. 
     for (let i = 0; i < numJugadas; i++) {
         //Solicitar al usuario que ingrese su jugada (número del 1 al 3 para piedra, papel, o tijera)
         //Comprobar que el valor ingresado sea 1,2 o 3. De no ser así dar mensaje de alerta y volver a solicitar dato.
-        var optJugador;
+        var optJugador=0;
+        /* Código usado antes de implementar Click
         do {
             optJugador=prompt("Escoja 1=Piedra      2=Papel     3=Tijera");
             if (optJugador<1 || optJugador>3) {
                 alert("Debe escoger una de las opciones indicadas")
             }
-        } while (optJugador<1 || optJugador>3);  
+        } while (optJugador<1 || optJugador>3);*/
+        while (optJugador==0) {
+            console.log("Esperando a que Jugador seleccione su opción...")
+        }
+        $("#piedra").click(function(){
+            optJugador=1
+        })
+        $("#papel").click(function(){
+            optJugador=2
+        })
+        $("#tijera").click(function(){
+            optJugador=3
+        })
         console.log(`El valor escogido por el jugador es ${optJugador}`)
         //Generar jugada de la máquina con función Math.random(), que genera valores entre [0,1)
         var optMaquina=Math.floor(Math.random()*3+1);
@@ -85,14 +99,32 @@ function cachipun(){
         }
     }
 
+    //Muestra de resultados finales
 
+    //Limpiar y volver a jugar
+    $("#again").css("display","block")
 }
 
 //Llamado de la función
 $(function(){
-    cachipun()
-});
+    //Detener animación de selectores al entrar a la caja que los contiene.
+    $(".selector").hover(function() {
+        $(".options i").toggleClass("animationOff");
+      }
+    );
+    //Iniciar Juego//
+    $("#start").click(function(){
+        $(this).css("display","none");
+        $(".selector").css("display","inline-block");
+        cachipun();
+    })
+    //Volver a jugar//
+    $("#again").click(function(){
+        $(this).css("display","none");
+        cachipun();
+    })
 
+});
 
 
 
